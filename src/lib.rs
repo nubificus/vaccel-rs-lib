f67713d3c673d382a0721e1f3554ba5ab62c91dc
pyrromanis <gpyrros@proton.me>
2023-03-14 23:37:04 +0000

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,108 @@
+use anyhow::{bail, Result};
+use dns_lookup::lookup_host;
+use std::fmt::Write;
+use tokio::{fs, process::Command};
+
+pub const VSOCK_PREFIX: &str = "vsock://";
+pub const UNIX_PREFIX: &str = "unix://";
+pub const TCP_PREFIX: &str = "tcp://";
+
+#[derive(Debug)]
+pub struct Agent {
+    agent_path: String,
+    endpoint: String,
+    debug: String,
+    backends: String,
+    backends_library: String,
+}
+
+impl Agent {
+    pub async fn create(
+        agent_path: String,
+        endpoint: String,
+        debug: String,
+        backends: String,
+        backends_library: String,
+    ) -> Agent {
+        Agent {
+            agent_path,
+            endpoint,
+            debug,
+            backends,
+            backends_library,
+        }
+    }
+    pub async fn start(&self) -> Result<()> {
+        let mut cmd = Command::new(&self.agent_path);
+        //println!("Endpoint: {}",&endpoint);
+        cmd.args(["-a", &self.endpoint]);
+        cmd.env("VACCEL_DEBUG_LEVEL", &self.debug);
+        let mut path = String::default();
+        for b in self.backends.split(',') {
+            write!(&mut path, "{}libvaccel-{}.so:", &self.backends_library, b);
+        }
+        path.pop();
+        cmd.env("VACCEL_BACKENDS", path);
+        let mut child = cmd.spawn()?;
+        match child.id() {
+            Some(id) => {
+                println!("VACCEL SPAWNED with id: {}", id);
+            }
+            None => {
+                let exit_status = child.wait().await?;
+                bail!("VACCEL BAD SPAWN with exit status: {:?}", exit_status);
+            }
+        };
+        child.wait().await?;
+        Ok(())
+    }
+}
+pub async fn construct_vsock(source: String, port: String) -> Result<String> {
+    let path = [&source, ":", &port].concat();
+    let full_path = [VSOCK_PREFIX, &source, ":", &port].concat();
+    match fs::remove_file(&path).await {
+        Ok(_) => {
+            //    println!("Previous vsock deleted")
+        }
+        Err(e) if e.kind() == std::io::ErrorKind::NotFound => {
+            //    println!("No previous vsock")
+        }
+        Err(e) => bail!("failed to remove vsock with error: {:?}", e),
+    }
+    Ok(full_path)
+}
+pub async fn construct_unix(source: String, port: String) -> Result<String> {
+    let path = [&source, "_", &port].concat();
+    let full_path = [UNIX_PREFIX, &path].concat();
+    match fs::remove_file(&path).await {
+        Ok(_) => {
+            //    println!("Previous unix socket deleted")
+        }
+        Err(e) if e.kind() == std::io::ErrorKind::NotFound => {
+            //    println!("No previous unix socket")
+        }
+        Err(e) => bail!("failed to remove unix socket with error: {:?}", e),
+    }
+    Ok(full_path)
+}
+pub async fn construct_tcp(arg_source: String, port: String) -> Result<String> {
+    let mut source = arg_source;
+    let mut dns: Vec<std::net::IpAddr> = vec![];
+    //FIXME: better check
+    if !source.contains('.') {
+        dns = lookup_host(&source).unwrap();
+        source = dns[0].to_string();
+    }
+    let path = [&source, ":", &port].concat();
+    let full_path = [TCP_PREFIX, &source, ":", &port].concat();
+    match fs::remove_file(&path).await {
+        Ok(_) => {
+            //    println!("Previous tcp socket deleted")
+        }
+        Err(e) if e.kind() == std::io::ErrorKind::NotFound => {
+            //    println!("No previous tcp socket")
+        }
+        Err(e) => bail!("failed to remove tcp socket with error: {:?}", e),
+    }
+    Ok(full_path)
+}
